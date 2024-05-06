09482e9da3e89fe4c1b7789448b76551e16e4072
dsempresa <130731804+dsempresa@users.noreply.github.com>
2024-05-06 12:11:38 -0300

    Create prova004.py

---
+++ prova_aula004/prova004.py
@@ -0,0 +1,15 @@
+# [PY-A04]Escreva uma função em Python que recebe dois números como argumentos e retorna o maior entre eles.
+
+# a) Implemente a função com o nome "maior_numero" e utilizando condicionais.
+
+# b) Implemente a mesma função, porém utilizando a função "max".
+
+def maior_numero(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+    
+
+def maior_numero(a, b):
+    return max(a, b)
